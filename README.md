ba0f74fe58a197f6fcbdb6c7014aea579f641743
estebanarriaga <estebanarriaga200@gmail.com>
2024-03-16 06:07:35 +0100

    Safe commit

--- README.md
+++ README.md
@@ -1,36 +1,64 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+To make a build of a Next.js project for non-Next.js developers, follow these steps. This guide assumes you have Node.js installed on your computer. If not, install it from [Node.js official website](https://nodejs.org/).
 
-## Getting Started
+# Building a Next.js Project
 
-First, run the development server:
+This guide will help you build a Next.js application, even if you're not familiar with Next.js.
+
+## Prerequisites
+
+- Ensure Node.js is installed on your system. You can check by running `node -v` in your terminal. If it's not installed, download and install it from [https://nodejs.org/](https://nodejs.org/).
+
+## Steps to Build
+
+1. **Clone the project** (if you have the repository URL) or **download the project files** to your local machine.
+
+2. **Open a terminal** and navigate to the project's root directory.
+
+3. **Install dependencies** by running the following command:
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+npm install
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+or if you prefer using Yarn:
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+```bash
+yarn
+```
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+4. **Build the project** by executing:
 
-## Learn More
+```bash
+npm run build
+```
 
-To learn more about Next.js, take a look at the following resources:
+or if you're using Yarn:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+```bash
+yarn build
+```
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+This command compiles your application and optimizes it for production. The output will be in the `.next` directory.
 
-## Deploy on Vercel
+5. **Start the application in production mode** by running:
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+```bash
+npm start
+```
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+or with Yarn:
+
+```bash
+yarn start
+```
+
+This serves your application on `http://localhost:3000` (or a different port if you've configured it).
+
+## Notes
+
+- The `build` step is crucial for preparing your application for production. It ensures that your site is optimized for performance.
+- Starting the application with `npm start` or `yarn start` after building is necessary to run your Next.js application in production mode.
+- If you encounter any issues, check the official Next.js documentation at [https://nextjs.org/docs](https://nextjs.org/docs) for troubleshooting and detailed guides.
+```
+
+This markdown guide simplifies the process of building and running a Next.js application, making it accessible to those unfamiliar with Next.js or web development frameworks in general.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,353 @@
 import Image from "next/image";
+import { UserForm } from "../components/form";
+import { Input } from "@/components/ui/input";
 
-export default function Home() {
+const meal_plan = {
+  options: 1,
+  meals: [
+    {
+      day: 0,
+      breakfast: {
+        description: "Oatmeal with fruits",
+        ingredients: [
+          {
+            name: "Oatmeal",
+            items: [
+              {
+                s_name: "dia", // Super
+                price: 2,
+                url: "https://example.com/oats", // Url producto
+              },
+              {
+                s_name: "carrefour",
+                price: 0.5,
+                url: "https://example.com/banana",
+              },
+              {
+                s_name: "mercadona",
+                price: 0.75,
+                url: "https://example.com/apple",
+              },
+            ],
+          },
+        ],
+      },
+      lunch: {
+        description: "Chicken Salad",
+        ingredients: [
+          {
+            name: "Salad",
+            items: [
+              {
+                s_name: "Chicken Breast",
+                price: 5,
+                url: "https://example.com/chicken-breast",
+              },
+              {
+                s_name: "Lettuce",
+                price: 1,
+                url: "https://example.com/lettuce",
+              },
+              {
+                s_name: "Tomato",
+                price: 0.5,
+                url: "https://example.com/tomato",
+              },
+            ],
+          },
+        ],
+      },
+      dinner: {
+        description: "Pasta with Tomato Sauce",
+        ingredients: [
+          {
+            name: "Pasta Dish",
+            items: [
+              {
+                s_name: "Pasta",
+                price: 1.5,
+                url: "https://example.com/pasta",
+              },
+              {
+                s_name: "Tomato Sauce",
+                price: 2,
+                url: "https://example.com/tomato-sauce",
+              },
+              {
+                s_name: "Parmesan",
+                price: 3,
+                url: "https://example.com/parmesan",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      day: 1,
+      breakfast: {
+        description: "Pancakes with syrup",
+        ingredients: [
+          {
+            name: "Pancakes",
+            items: [
+              {
+                s_name: "Flour",
+                price: 0.5,
+                url: "https://example.com/flour",
+              },
+              {
+                s_name: "Eggs",
+                price: 0.3,
+                url: "https://example.com/eggs",
+              },
+              {
+                s_name: "Maple Syrup",
+                price: 3,
+                url: "https://example.com/maple-syrup",
+              },
+            ],
+          },
+        ],
+      },
+      lunch: {
+        description: "Vegetable Stir Fry",
+        ingredients: [
+          {
+            name: "Stir Fry",
+            items: [
+              {
+                s_name: "Bell Peppers",
+                price: 1,
+                url: "https://example.com/bell-peppers",
+              },
+              {
+                s_name: "Broccoli",
+                price: 1.2,
+                url: "https://example.com/broccoli",
+              },
+              {
+                s_name: "Chicken",
+                price: 4,
+                url: "https://example.com/chicken",
+              },
+            ],
+          },
+        ],
+      },
+      dinner: {
+        description: "Fish Tacos",
+        ingredients: [
+          {
+            name: "Tacos",
+            items: [
+              {
+                s_name: "Tilapia",
+                price: 6,
+                url: "https://example.com/tilapia",
+              },
+              {
+                s_name: "Corn Tortillas",
+                price: 2,
+                url: "https://example.com/corn-tortillas",
+              },
+              {
+                s_name: "Avocado",
+                price: 1.5,
+                url: "https://example.com/avocado",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      day: 2,
+      breakfast: {
+        description: "Greek Yogurt with Honey and Nuts",
+        ingredients: [
+          {
+            name: "Yogurt Breakfast",
+            items: [
+              {
+                s_name: "Greek Yogurt",
+                price: 2,
+                url: "https://example.com/greek-yogurt",
+              },
+              {
+                s_name: "Honey",
+                price: 1.5,
+                url: "https://example.com/honey",
+              },
+              {
+                s_name: "Mixed Nuts",
+                price: 2.5,
+                url: "https://example.com/mixed-nuts",
+              },
+            ],
+          },
+        ],
+      },
+      lunch: {
+        description: "Quinoa Salad",
+        ingredients: [
+          {
+            name: "Salad",
+            items: [
+              {
+                s_name: "Quinoa",
+                price: 2,
+                url: "https://example.com/quinoa",
+              },
+              {
+                s_name: "Cucumber",
+                price: 0.7,
+                url: "https://example.com/cucumber",
+              },
+              {
+                s_name: "Feta Cheese",
+                price: 2,
+                url: "https://example.com/feta-cheese",
+              },
+            ],
+          },
+        ],
+      },
+      dinner: {
+        description: "BBQ Chicken Pizza",
+        ingredients: [
+          {
+            name: "Pizza",
+            items: [
+              {
+                s_name: "Pizza Dough",
+                price: 1,
+                url: "https://example.com/pizza-dough",
+              },
+              {
+                s_name: "BBQ Sauce",
+                price: 1.5,
+                url: "https://example.com/bbq-sauce",
+              },
+              {
+                s_name: "Chicken",
+                price: 4,
+                url: "https://example.com/chicken-bbq",
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
+
+interface IngredientItem {
+  s_name: string;
+  price: number;
+  url: string;
+}
+
+interface Ingredient {
+  name: string;
+  items: IngredientItem[];
+}
+
+interface Meal {
+  description: string;
+  ingredients: Ingredient[];
+}
+
+interface DayMealPlan {
+  day: number;
+  breakfast: Meal;
+  lunch: Meal;
+  dinner: Meal;
+}
+
+interface MealPlan {
+  options: number;
+  meals: DayMealPlan[];
+}
+
+// Para iconos
+export default async function Home() {
+  // let data;
+  // try {
+  //   const req = await fetch("http://10.19.200.106:8080/api/search", {
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       name: "Make me a vegetarian meal with products of somewere ",
+  //     }),
+  //   });
+  //   data = await req.json();
+  //   console.log(data);
+  // } catch (err) {
+  //   console.error(err);
+  // }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="mx-20 my-10 h-full max-w-full">
+      {/* <UserForm /> */}
+      <div className="w-full flex flex-row justify-end">
+        <div className="w-2/5 rounded-xl flex flex-row justify-start bg-slate-100 p-3 text-gray-500">
+          Make me a vegetarian meal with products of carrefour.
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col justify-start mt-10"> 
+        {meal_plan.meals.map((mealDay, index) => (
+          <div
+            className=" w-full"
+            key={index}
+          >
+            <h2 className="scroll-m-20 text-3xl font-bold tracking-tight first:mt-0 text-[#253D4E]">
+              Day {mealDay.day}
+            </h2>
+            {["breakfast", "lunch", "dinner"].map((mealType) => (
+              <div key={mealType}>
+                <div className="flex flex-row items-center justify-start gap-3">
+                  <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-[#253D4E]">
+                    {mealType.charAt(0).toUpperCase() + mealType.slice(1)}:
+                  </h3>
+                  <span className=" text-sm px-2 py-1 rounded-md text-[#62E4A3] bg-[#253D4E]">
+                    {mealDay[mealType].description}
+                  </span>
+                </div>
+                {mealDay[mealType].ingredients.map((ing: Ingredient) => (
+                  <>
+                    <h4
+                      key={ing.name}
+                      className="scroll-m-20 text-xl font-semibold tracking-tight"
+                    >
+                      {ing.name}
+                    </h4>
+                    <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                      {ing.items.map((item: IngredientItem) => (
+                        <li key={item.s_name}>
+                          <a
+                            href={item.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {item.s_name}: ${item.price}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="fixed botton-4 w-full">
+        <h4 className="bg-transparent">Do you want to ask something else?</h4>
+        <Input placeholder="Ask me something..." type="text" />
       </div>
-    </main>
+    </div>
   );
 }
